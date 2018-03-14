b8dc453c53cdf8366ded57ae48cc7de6eacd4202
owen151128 <owen151128@gmail.com>
2018-03-14 22:36:27 +0900

    MainActivity 위치 변경
    UI복잡도를 고려하여 Package 별로 Activity 나눌수 있게 설계

--- app/src/main/java/kr/owens/playword/ui/MainActivity.java
+++ app/src/main/java/kr/owens/playword/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package kr.owens.playword;
+package kr.owens.playword.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import kr.owens.playword.R;
 
 public class MainActivity extends AppCompatActivity {
 
